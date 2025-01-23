eb67e78263a34a3a993338d0cda371484e8a4780
NegasaReta <167341232+NegasaReta@users.noreply.github.com>
2025-01-23 23:08:21 +0300

    Update aboutpageservlet.java

--- src/main/java/aboutpageservlet.java
+++ src/main/java/aboutpageservlet.java
@@ -1,1 +1,14 @@
-
+package com.example.fullstack;
+import java.io.*;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+@WebServlet("/about")
+public class aboutpageservlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/aboutpage.jsp");
+        rd.forward(req,resp);
+    }
+    }
